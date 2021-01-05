3f25fb7ff0df0d133a1eee39a1b32af31018cdc3
Oleksandr Bratashov <abratashov@gmail.com>
2021-01-05 21:26:45 +0200

    Full refactoring

--- gemsi.rb
+++ gemsi.rb
@@ -1,87 +1,81 @@
-require 'awesome_print'
-require 'terminal-table'
+# frozen_string_literal: true
 
 class Gemsi
+  GEM_LINE_PATTERN = /^\s*(gem|group|ruby)/.freeze
+  GEM_NAME_PATTERN = /^\s*gem\s*[\'\"]([-_\w\d]*)[\'\"]/.freeze
+
   class << self
     def main
-      strings = parse_gemfile
-      gems_strings = descriptions_hash(strings)
-      print_description_table(gems_strings)
+      gems = parsed_gems
+      gem_names_arr = []
+      gems.each do |gem|
+        next if !gem[:is_parsed] || gem[:name].nil?
+
+        gem_names_arr << gem[:name]
+        gem[:spec] = gems_descriptions.detect { |description| description[:name] == gem[:name] }
+      end
+      name_size = gem_names_arr.max_by(&:length).size
+      print_descriptions(gems, name_size)
 
       puts
-      puts "Gemsi generator tool, https://github.com/phlowerteam/gemsi, MIT License, (c) 2014 PhlowerTeam"
+      puts "Gemsi generator tool, https://github.com/phlowerteam/gemsi, MIT License, (c) 2020 PhlowerTeam"
       puts
     end
 
     private
 
-    def parse_gemfile(path=nil)
-      gemfile = path ? path : 'Gemfile'
+    def parsed_gems(path = nil)
+      gemfile = path || 'Gemfile'
       IO.read(gemfile).split("\n").map do |string|
-        string.match(/^\s*(gem|group|ruby)/) ? {text: string, is_parsed: true} : {text: string, is_parsed: false}
+        is_parsed = string.match(GEM_LINE_PATTERN) ? true : false
+        name = is_parsed ? string.match(GEM_NAME_PATTERN)&.send(:[], 1) : nil
+        { text: string, name: name, is_parsed: true }
       end
     end
 
-    def descriptions_hash(strings=[])
-      results = []
-      strings.each do |string|
-        substrings = string[:text].match(/^\s*gem\s*\'([-_\w\d]*)\'/)
-        gem_name = substrings ? substrings[1] : nil
-        if substrings && gem_name
-          begin
-            raise if gem_name.match(/(swagger|directory|ads|shop|activeadmin)/)
-
-            spec = Gem::Specification::find_by_name(gem_name)
-            if spec
-              results << get_description(gem_name, spec).merge({is_parsed: string[:is_parsed]})
-            end
-          rescue
-            results << string
-          ensure
-          end
-        else
-          results << string
-        end
-      end
-      results
+    def gems_descriptions
+      @gems_descriptions ||= Gem::Specification.all.map { |spec| build_description(spec) }
     end
 
-    def get_description(gem_name, spec)
+    def build_description(spec)
       result = {
-        gem_name:      gem_name,
-        version:       spec.version.to_s,
-        summary:       spec.summary,
-        description:   spec.description,
-        homepage:      spec.homepage,
-        github_search: "https://github.com/search?q=#{gem_name}&ref=cmdform"
+        name: spec.name,
+        version: spec.version.to_s,
+        summary: spec.summary,
+        description: spec.description,
+        homepage: spec.homepage,
+        github_search: "https://github.com/search?q=#{spec.name}&ref=cmdform"
       }
 
       result[:full_description] = if result[:description] == result[:summary]
-         result[:description]
-      else
-         "#{result[:description]} #{result[:summary]}"
+                                    result[:description]
+                                  else
+                                    "#{result[:description]} #{result[:summary]}"
       end
-      result[:github_search] = result[:homepage].match(/github/) ? " ": "https://github.com/search?q=#{gem_name}&ref=cmdform"
+      result[:github_search] = result[:homepage]&.match(/github/) ? ' ' : "https://github.com/search?q=#{spec.name}&ref=cmdform"
       result
     end
 
-    def print_description_table(strings, short=false)
-      table = Terminal::Table.new
-      strings.each do |str|
-        if str[:is_parsed] && str[:text].to_s != ''
-          ap str[:text]
-          puts table unless table.rows.empty?
-          table = Terminal::Table.new
-        elsif str[:is_parsed]
-          if short
-            table.add_row ["#{str[:gem_name]} #{str[:version]}", str[:summary].scan(/.{0,80}/).join("\n")]
-          else
-            table.add_row ["#{str[:gem_name]} #{str[:version]}", str[:full_description].scan(/.{0,80}/).join("\n") + "#{str[:homepage]}\n#{str[:github_search]}\n\n"]
-            table.add_row [" ", " "]
+    def print_descriptions(gems, name_size = 20)
+      gems.each do |gem|
+        next unless gem[:is_parsed]
+
+        puts gem[:name].ljust(name_size).to_s if gem[:name]
+        if gem[:spec].nil?
+          gem[:text].scan(/.{0,80}/).each do |part|
+            puts "#{' ' * name_size}#{part}"
+          end
+        else
+          puts "#{' ' * name_size}#{gem[:spec][:version]} "
+          gem[:spec][:full_description].scan(/.{0,80}/).each do |part|
+            puts "#{' ' * name_size}#{part}"
+          end
+          if gem[:spec][:homepage]
+            puts "#{' ' * name_size}#{gem[:spec][:homepage]}\n"
+            puts "#{' ' * name_size}#{gem[:spec][:github_search]}\n"
           end
         end
       end
-      puts table unless table.rows.empty?
     end
   end
 end
